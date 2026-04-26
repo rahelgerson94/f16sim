8524b0ad749e05720402185a362c45555b58b759
Rahel Gerson <rahelgerson94@gmail.com>
2026-04-26 15:16:08 -0500

    ici

--- matlab/LinearAnalysis/test/testCalcDerivsLon.m
+++ matlab/LinearAnalysis/test/testCalcDerivsLon.m
@@ -1,11 +1,37 @@
-Vt = 1000; 
-alpha = 30; 
-theta = 0;
-test = CalcDerivsLon(Vt, alpha, theta);
 
-%output of calcTrimSteadyFlight
-x0 = [ 30.48 -0.23 -0.00 0.17 0.09 -304.80 ]'; %english units
-u0 = [0.06 -0.11 -0.11 -0.06]'; 
+cd ..; setupProject; cd LinearAnalysis;
+f16NominalTrim; % get xe, ue into the workspace
+test = CalcDerivsLon(xe, ue);
+dt = c.dt;
+SIM_DURATION = 5;
 
-uDot = 
+AIL_IDX = 1;
+ELE_IDX = 2;
+RUD_IDX = 3;
+%% simulation parameters
+SIM_DURATION = 5;
+SIM_DURATION_SAMPLES = ceil(SIM_DURATION/dt);
+APPLICATION_START_TIME = 2; 
+APPLICATION_START_IDX = ceil(APPLICATION_START_TIME/dt); 
 
+STEP_DURATION = 1;
+STEP_DURATION_SAMPLES = 1*c.dt;
+
+X_LON = zeros(SIM_DURATION_SAMPLES,6);
+U = zeros(2,SIM_DURATION_SAMPLES);
+test.setInitialState(xe);
+
+for i = 1:SIM_DURATION_SAMPLES
+    test.update(ue);
+    X_LON(i,:) = test.getState();
+    
+end
+
+ [UVW, PQR, Q, NED] = unpackX(X_LON);
+ plotStatesLon(time, U,W,Q,TH);
+
+ function [U,W,Q,TH, N,D] = unpackXlon(Xlon)
+    U = X(:, 1); W = X(:, 2);
+    Q = X(:, 3); TH = X(:, 4);
+    N = X(:, 5); D = X(:, 6);
+    end
