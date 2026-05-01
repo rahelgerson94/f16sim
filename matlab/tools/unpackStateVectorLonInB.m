520910d819ee70b6dd047e237179d85a7156012e
Rahel Gerson <rahelgerson94@gmail.com>
2026-05-01 16:33:14 -0500

    specified frame forunpackStateVector

---
+++ matlab/tools/unpackStateVectorLonInB.m
@@ -0,0 +1,9 @@
+function [u,w,q,theta,n,d] = unpackStateVectorLonInB(X)
+    u = X(1);
+    w = X(2);
+    q = X(3);
+    theta = X(4);
+    n = X(5);
+    d = X(6);
+    
+end
---
+++ matlab/tools/unpackStateVectorLonInW.m
@@ -0,0 +1,7 @@
+function [Vt, alpha, q, theta] = unpackStateVectorLonInB(X)
+    Vt = X(1);
+    alpha = X(2);
+    q = X(3);
+    theta = X(4);
+    
+end
