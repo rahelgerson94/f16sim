96e1b00d872e1099d0507b9a80d3a00a2329e5e5
Rahel Gerson <rahelgerson94@gmail.com>
2026-05-09 08:40:05 -0500

    fixed ordering issue

--- matlab/tools/unpackStateVectorLonInW.m
+++ matlab/tools/unpackStateVectorLonInW.m
@@ -1,8 +1,8 @@
 function [Vt, alpha, q, theta, n,d] = unpackStateVectorLonInB(X)
     Vt = X(1);
-    alpha = X(2);
-    q = X(3);
-    theta = X(4);
+    theta = X(2);
+    alpha = X(3);
+    q = X(4);
     n = X(5);
     d = X(6);
     
