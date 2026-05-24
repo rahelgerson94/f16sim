0518a8ada9312eb49d70e345d26764641e1c7c34
Rahel Gerson <rahelgerson94@gmail.com>
2026-05-24 09:02:39 -0500

    cleanup

--- matlab/AeroModel/test/testAeroModel.m
+++ matlab/AeroModel/test/testAeroModel.m
@@ -8,7 +8,11 @@ addpath(fullfile(matlabRoot, 'AeroModel'));
 addpath(fullfile(matlabRoot, 'tools'));
 addpath(fullfile(matlabRoot, 'KinematicMath'));
 addpath(fullfile(matlabRoot, 'LinearAnalysis'));
-f16NominalTrim; %xe = [u; w; q; theta; 0;-h];  xeInW= [VtFt alphaDeg  q thetaDeg 0 -h]';
+
+xeInW = load([matlabRoot '/generated/xeInW.mat']).xeInW;
+ue = load([matlabRoot '/generated/ue.mat']).ue;
+fprintf("ue : [δ_e, δ_t] = [%.2f   %.2f]\n", ue)
+fprintf("[Vt (ft),   θ (deg), α (deg), q (deg)]  = [%.2f    %.2f     %.2f     %.2f ]\n", xeInW(1), xeInW(2), xeInW(3), xeInW(4) );
 Vt = xeInW(1)*c.FT2M;
 wB_rad = [0 ;  xeInW(3); 0];
 aert = [0; ue(1); 0; ue(2)];
@@ -24,33 +28,55 @@ alphaVec = linspace(-20, 80, N);
 betaVec = linspace(-30, 30, N);
 dhVec = linspace(-25, 25, N);
  
-CfinB = zeros(N,3);
-CmInB = zeros(N,3);
-CfInW = zeros(N,3);
-CmInW = zeros(N,3);
+CfTotinB = zeros(N,3);
+CmTotInB = zeros(N,3);
+CfTotInW = zeros(N,3);
+CmTotInW = zeros(N,3);
 CmDampingInB = zeros(N,3);
+CmInB =  zeros(N,3);
+DCL = zeros(N,3);
+deltaCmDampingInB= zeros(N,3);
 for i = 1:N
-    alpha = alphaVec(i);  dh = dhVec(i);
+    alphaDeg = alphaVec(i);  dh = dhVec(i);
     rho =  rhoFromAlt(hM);
-    [cxStruct, cmStruct, cmDampingStruct] = aeromodel.updateCoeffs(alpha, 0, Vt, wB_rad, rho, aert);
+    [cxStruct, cmStruct, cmDampingStruct] = aeromodel.updateCoeffs(alphaDeg, 0, Vt, wB_rad, rho, aert);
 
     Cf=[cxStruct.Cx; cxStruct.Cy; cxStruct.Cz];
-    Cm = [cmStruct.Cl; cmStruct.Cm; cmStruct.Cn];
+    CmTot = [cmStruct.Cl; cmStruct.Cm; cmStruct.Cn];
+    
+    CfTotinB(i,:) = Cf;
+    CmTotInB(i,:)=CmTot;
 
-    CfinB(i,:) = Cf;
-    CmInB(i,:)=Cm;
     % Convert the coefficient vectors with radians because body2wind uses sin/cos.
-    alphaRad = alpha*c.DEG2RAD;
+    alphaRad = alphaDeg*c.DEG2RAD;
     % multiply by -1 because D is along -xb,
     % C along -yb, L along -zb
-    CfInW(i,:) = body2windCoeffs(Cf, alphaRad, 0);
-    CmInW(i,:)= body2windCoeffs(Cm, alphaRad, 0);
+    CmInB(i,:)= aeromodel.Cm(alphaDeg, 0,0);
+    Cx = aeromodel.Cx(alphaDeg,0,0);
+    Cz = aeromodel.Cz(alphaDeg,0,0);
+    Cy=aeromodel.Cy(alphaDeg,0);
+    DCL(i,:)=body2windCoeffs([Cx Cy Cz], alphaRad, 0);
+    CfTotInW(i,:) = body2windCoeffs(Cf, alphaRad, 0);
+    CmTotInW(i,:)= body2windCoeffs(CmTot, alphaRad, 0);
+    CmDampingInB(i,:) =  [cmDampingStruct.Cl; ...
+                                        cmDampingStruct.Cm; ...
+                                        cmStruct.Cn];...
+    deltaCmDampinginB(i,:) = aeromodel.Cm(alphaDeg, 0,0);
 
-    
-
-end
+end 
 %plotForceMomentDashboard(alphaVec, CfinB, CmInB);
-plotForceMomentDashboard(alphaVec, CfInW, CmInW);
+plotVector3(alphaVec, DCL, "C_d, C_y, C_L vs α (deg)");
+
+figure; plot(alphaVec,CmInB);
+plotForceMomentDashboard(alphaVec, CfTotInW, CmTotInW);
+figure; labels ={"l", "m", "n"}; colors = {"r", "b", "g"};
+for i = 1:3
+    plot(alphaVec, CmDampingInB(:,i), "DisplayName", sprintf("C_%s", labels{i}), ...
+        "LineWidth",1.5,...
+    "Color" , colors{i}); hold on;
+end
+legend; grid on; xlabel("α (deg)");
+hold off;
 % tiledlayout(1,3); L = 1.5;
 % nexttile;
 % % Plot against the full alpha sweep, not the last scalar alpha from the loop.
