6fbe4d12d6090d4371432f6b329e8c58d27edd38
Rahel Gerson <rahelgerson94@gmail.com>
2026-05-24 11:49:30 -0500

    plotting root locus and step response

--- matlab/LinearAnalysis/analysis/longitudinal.m
+++ matlab/LinearAnalysis/analysis/longitudinal.m
@@ -18,8 +18,21 @@ tfs.th2ele = tfs_(c.lon.TH_IDX,1);
 tfs.alf2ele = tfs_(c.lon.ALF_IDX,1);
 tfs.q2ele = tfs_(c.lon.Q_IDX,1);
 
-% tfs_ = tf(ss(A, B(:,c.lon.THTL_IDX), eye(4), zeros(4,1)));
-% tfs.vt2thtl = tfs_(c.lon.VT_IDX,1);
-% tfs.th2thtl = tfs_(c.lon.TH_IDX,1);
-% tfs.alf2thtl = tfs_(c.lon.ALF_IDX,1);
-% tfs.q2thtl = tfs_(c.lon.Q_IDX,1);
+%θ (deg), α (deg), q (deg)]
+uName = "δ_e";
+stateNames = {"V_t", "θ", "α", "q"};
+fieldNames = {"vt2ele", "th2ele", "alf2ele", "q2ele"};
+for i = 1:c.lon.nStates
+    h = figure;
+    h.Name = sprintf("%s to %s: root locus and step response", stateNames{i}, uName);
+    
+    % Keep root locus in its own axes because it uses the complex plane.
+    subplot(1, 2, 1);
+    rlocus(tfs.(fieldNames{i})); grid on;
+    title("Root Locus");
+    
+    % Keep step response in its own axes because it uses time history.
+    subplot(1, 2, 2);
+    step(tfs.(fieldNames{i})); grid on;
+    title("Step Response");
+end
